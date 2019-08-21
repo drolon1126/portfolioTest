45febe9baf1c099cfc79d462a04d3760a3307193
danRolon <daniel.rolon@yahoo.com>
2019-08-21 17:44:50 -0500

    removed accidental header

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,7 +8,10 @@ const Projects = props => {
     <p>Here you will find various projects I've worked on.</p>
     <hr/>
     <ul class="grid">
-      <li class="small"> <Microlink url='https://anime-repo-h5zq95ubi.now.sh/' media='screenshot' /></li>
+      <li class="small">
+        <h4 className='pTittle'>Animu Tracker</h4>
+        <Microlink url='https://anime-repo-h5zq95ubi.now.sh/' media='screenshot' size='normal' lazy  />
+      </li>
     </ul>
     <hr/>
   </div>
